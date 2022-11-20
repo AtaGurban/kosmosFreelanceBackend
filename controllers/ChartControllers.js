550c2f72d6cab243ba1877a5e84b45d048f3902f
Ata Gurban <ata-g@mail.ru>
2022-11-20 21:22:29 +0500

    dsds

--- controllers/ChartControllers.js
+++ controllers/ChartControllers.js
@@ -1,36 +1,45 @@
 const { HistoryBargain } = require("../models/TablesExchange/tableHistoryBargain");
 const { Op } = require("sequelize");
 const { Market } = require("../models/TablesExchange/tableMarket");
+const chartPeriod = require("../service/chartPeriod");
 
 class ChartControllers {
   async list(req, res, next) {
-    const {command, currencyPair, start, end, period} = req.query
+    const {command, currencyPair, start, end, period: period} = req.query
+    // let periodMs = Math.round(((+period) * 1000) / 70)
+    let periodMs = period
     if (command === 'returnChartData'){
-        if (currencyPair && start && end && period){
+        if (currencyPair && start && end && periodMs){
             const tradeID = (await Market.findOne({where:{pair:currencyPair}})).id
             const historyItems = await HistoryBargain.findAll({where : {date : {[Op.between] : [new Date(+start), new Date(+end)]},tradeID }})
             let periods = [];
             let bool = true
-            let periodStart = (+start);
-            let periodEnd = (+start) + (+period)
-            do {
-                let period = historyItems.filter((j)=>{
-                    let dateToMs = new Date(j.date).getTime()
-                    return ((dateToMs > periodStart) && (dateToMs < periodEnd))
-                })
-                // console.log(period.length);
-                if (period.length > 0){
-                    periods.push(period)
-                    periodStart += period
-                    periodEnd += period 
-                } else {
-                    periodStart += period
-                    periodEnd += period  
-                    bool = false
-                }
-                console.log(periodEnd);  
-              } while (periodEnd < end);
-              return res.json(periods)
+            let counter = 0;
+            if (historyItems.length > 0){
+              let periodStart = (+new Date(historyItems[0].date)); 
+              let periodEnd = periodStart + (+periodMs)
+              do {
+                  let periodArr = historyItems.filter((j)=>{
+                      let dateToMs = new Date(j.date).getTime()
+                      return ((dateToMs >= periodStart) && (dateToMs <= periodEnd))
+                  })
+                  // console.log(period.length);
+                  if (periodArr.length > 0){
+                      periods.push({periodArr, date: periodEnd})
+                      periodStart += (+periodMs)
+                      periodEnd += (+periodMs)
+                      counter++
+                      if (counter === historyItems.length){
+                        bool = false
+                      } 
+                  } else {
+                      periodStart += (+periodMs)
+                      periodEnd += (+periodMs)    
+                  }  
+                } while ((periodEnd < end) && (bool)); 
+                // return res.json(periods)
+                return res.json(await chartPeriod(periods))
+            }
         }
     }
   }
---
+++ service/chartPeriod.js
@@ -0,0 +1,18 @@
+
+
+module.exports = async (periods) => {
+    let result = []
+    periods.map((i) => {
+        let prices = []
+        let volume = 0
+        i.periodArr.map((j) => {
+            prices.push((+j.total) / (+j.amount))
+            volume = volume + (+j.total)
+        })
+        let sortedPrices = prices.sort((a, b) => b - a);
+        let high = sortedPrices[0]
+        let low = sortedPrices[sortedPrices.length - 1]
+        result.push({ date: i.date, open: prices[0], close: prices[prices.length - 1], high, low, volume })
+    })
+    return result
+}
