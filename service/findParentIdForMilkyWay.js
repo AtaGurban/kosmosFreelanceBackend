5006aef268f39690da27d1dbbcea7146a755af13
Ata Gurban <ata-g@mail.ru>
2022-12-12 21:58:06 +0500

    fd

--- service/findParentIdForMilkyWay.js
+++ service/findParentIdForMilkyWay.js
@@ -1,22 +1,29 @@
 const { Matrix, Matrix_Table } = require("../models/models")
 
-const findParentIdForMilkyWay = async(level, userId)=>{
-    if (userId !== 1){
+const findParentIdForMilkyWay = async (level, userId) => {
+    if (level <= 15) {
         const matrixTemp = await Matrix.findAll({ include: { model: Matrix_Table, as: "matrix_table" } })
         const matrix = matrixTemp.filter((i, index) => {
             return ((i.matrix_table[0]?.typeMatrixId === level) && (i.matrix_table[0]?.count > 6))
         })
         const parentId = matrix[0]?.id
-        if (parentId){
-            return {parentId, typeMatrixId: level}
+        if (parentId) {
+            return { parentId, typeMatrixId: level }
         } else {
-            return findParentIdForMilkyWay(level + 1, userId)
+            const checkMatrixTable = await Matrix_Table.findAll({ where: { typeMatrixId: { [Op.gt]: level } } })
+            if (checkMatrixTable.length > 0) {
+                return findParentIdForMilkyWay(level + 1, userId)
+            } else {
+                return { parentId: null, typeMatrixId: level }
+            }
+
         }
-    } else if (level > 15) {
-        return {parentId: null, typeMatrixId: 1}
-    } else {
-        return {parentId: null, typeMatrixId: level}
     }
+    // else if (level > 15) {
+    //     return {parentId: null, typeMatrixId: 1}
+    // } else {
+    //     return {parentId: null, typeMatrixId: level}
+    // }
 
 }
 
